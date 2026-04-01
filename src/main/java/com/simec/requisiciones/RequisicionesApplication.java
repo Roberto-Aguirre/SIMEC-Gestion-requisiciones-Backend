d758c32a35edc184b5fc8125f42174473b8f0f9e
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2026-03-31 23:20:49 -0600

    Init project

---
+++ src/main/java/com/simec/requisiciones/RequisicionesApplication.java
@@ -0,0 +1,13 @@
+package com.simec.requisiciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequisicionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequisicionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simec/requisiciones/RequisicionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simec.requisiciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequisicionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
